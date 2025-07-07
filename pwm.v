b0f220478519253cf9342a2a575826b9825093b2
Zazamartins <isaacmrtnsss@gmail.com>
2025-07-07 15:14:10 -0300

    talvez ok

--- pwm.v
+++ pwm.v
@@ -6,6 +6,22 @@ module PWM (
     output reg pwm_out
 );
 
-//insira o seu código aqui
-    
+    reg [31:0] counter;
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            counter <= 0;
+            pwm_out <= 0;
+        end
+        else begin
+            if (counter >= period-1) begin
+                counter <= 32'b0;
+            end
+            else begin
+                counter <= counter + 1;
+            end
+            pwm_out <= (counter < duty_cycle) ? 1'b1 : 1'b0; 
+        end
+    end
+
 endmodule
--- servo.v
+++ servo.v
@@ -7,6 +7,42 @@ module servo #(
     output wire servo_out
 );
 
-//insira seu código aqui
+    wire [31:0] duty_cycle;
+    reg [31:0] contador; // 10 secs
+    reg condicional;
+
+    assign duty_cycle = (condicional) ? (PERIOD * 0.1) : (PERIOD * 0.05); // 10% e 5%
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            contador <= 0;
+            condicional <= 0;
+        end
+        else begin
+            if (contador >= (CLK_FREQ * 10) - 1) begin // 10 vezes o ciclo do clock
+                contador <= 0;
+            end
+            else begin
+                contador <= contador + 1;
+            end
+
+            if(contador < (CLK_FREQ * 5) - 1) begin //metade do tempo
+                condicional <= 0;
+            end
+            else begin
+                condicional <= 1;
+            end
+        end
+    end
+
+    PWM pwm_servo(
+        .clk(clk),
+        .rst_n(rst_n),
+        .duty_cycle(duty_cycle),
+        .period(PERIOD),
+        .pwm_out(servo_out)
+    );
+
+
 
 endmodule
